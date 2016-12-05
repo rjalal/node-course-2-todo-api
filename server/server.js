6718bf384a9c75d4fc55565751380f78153d9e48
Rami Jalal <Rami.Jalal@Colostate.edu>
2016-12-05 01:16:21 -0700

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+const {ObjectID} = require('mongodb');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -30,6 +32,21 @@ app.get('/todos',(req,res)=>{
     })
 });
 
+app.get('/todos/:id',(req,res)=>{
+    var id = req.params.id; 
+
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send();
+    };
+
+    Todo.findById(id).then((todo)=>{
+        if (!todo){
+            res.status(404).send()
+        };
+        res.send({todo});
+    }).catch((e)=> res.status(400).send());
+
+});
 
 app.listen(3000, ()=> {
     console.log('Started on port 3000');
